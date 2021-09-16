d23a487cfa743b43b4aea7636c04f68c254d97b8
root4root <root4root@gmail.com>
2021-09-17 01:53:38 +0300

    Fix warning: converting a string constant to 'char*'

--- SIM800.h
+++ SIM800.h
@@ -1,3 +1,32 @@
+/*
+   SIM800 - library for Simcom SIM800(L) module.
+   Project page: https://github.com/root4root/sim800
+*/
+
+/*
+MIT License
+
+Copyright (c) 2020 root4root
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <arduino.h>
 
 #ifndef SIMSTRBUFF
@@ -37,7 +66,7 @@ class SIM800
         
         char * handle();
         char * waitResponse();
-        void sendSMS(char *, char *);
+        void sendSMS(const char *, const char *);
         
 };
 
@@ -140,7 +169,7 @@ char * SIM800<T>::waitResponse()
 }
 
 template <typename T>
-void SIM800<T>::sendSMS(char *number, char *text)
+void SIM800<T>::sendSMS(const char *number, const char *text)
 {
     serial->println(F("AT+CMGF=1"));
 
